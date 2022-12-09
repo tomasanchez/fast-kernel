81ccf9687c85c20af01a2a8924f2392f30717df3
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-12-09 00:25:22 -0300

    refactor: add setup.py

---
+++ console/src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='console',
+    version='0.0.1',
+    description='A basic Kafka producer',
+    author='Tomas Sanchez',
+    author_email='tosanchez@frba.utn.edu.ar',
+    packages=['console'],
+)
