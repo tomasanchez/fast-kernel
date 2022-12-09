acf0841c2dc7ba132bd9631f5623de97b5b08270
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-12-09 01:11:47 -0300

    refactor: changer to AIOKafka

--- kernel/app/routers/instructions.py
+++ kernel/app/routers/instructions.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi import HTTPException
 
 from app.dependencies import get_kafka_service
 from app.services.kafka_service import KafkaService
@@ -6,14 +7,14 @@ from app.services.kafka_service import KafkaService
 router = APIRouter(
     prefix="/instructions",
     tags=["instructions"],
-    responses={400: {"description": "Invalid Instruction"}},
+    responses={500: {"description": "Kafka Connection Error"}},
 )
 
 
 @router.post("/")
 async def read(ks: KafkaService = Depends(get_kafka_service)) -> str:
     try:
-        ks.consume()
+        await ks.consume()
         return "OK"
     except Exception as e:
-        return str(e)
+        raise HTTPException(status_code=500, detail=str(e))
--- kernel/app/services/kafka_service.py
+++ kernel/app/services/kafka_service.py
@@ -1,8 +1,11 @@
-from typing import TypeVar
+import asyncio
+import json
+from typing import TypeVar, Type
 
-from kafka import KafkaConsumer
+from aiokafka import AIOKafkaConsumer
 
 from app.scehmas.base import CamelCaseModel
+from app.scehmas.instruction import Instruction
 
 T = TypeVar("T", bound=CamelCaseModel)
 
@@ -10,27 +13,40 @@ T = TypeVar("T", bound=CamelCaseModel)
 class JsonCamelCaseEncoder:
     enconding = "utf-8"
 
+    def __init__(self, kind: Type[T] = Instruction):
+        self.kind = kind
+
     def encode(self, obj: T) -> bytes:
         return obj.json(by_alias=True, exclude_none=True).encode(encoding=self.enconding)
 
     def decode(self, data: str) -> T:
-        return T.parse_raw(data, encoding=self.enconding)
+        return self.kind.parse_raw(data, encoding=self.enconding)
 
 
 class KafkaService:
     INSTRUCTION_KAFKA_TOPIC = "instructions"
     port = "29092"
-    encoder = JsonCamelCaseEncoder()
-    consumer = KafkaConsumer(
+    encoder = JsonCamelCaseEncoder(kind=Instruction)
+    loop = asyncio.get_event_loop()
+    consumer = AIOKafkaConsumer(
         INSTRUCTION_KAFKA_TOPIC,
         bootstrap_servers=f"localhost:{port}",
         auto_offset_reset='earliest',
         consumer_timeout_ms=10000,
         group_id=None,
-    )
+        loop=loop)
 
-    def consume(self):
-        print("Consuming...")
-        for message in self.consumer:
-            print(message.value.decode("utf-8"))
-        print("Done consuming")
+    async def consume(self):
+        await self.consumer.start()
+        try:
+            async for msg in self.consumer:
+                msg_decoded = json.loads(msg.value.decode(encoding="utf-8"))
+
+                if isinstance(msg_decoded, list):
+                    msg_decoded = [self.encoder.decode(data) for data in msg_decoded]
+                else:
+                    msg_decoded = self.encoder.decode(json.dumps(msg_decoded))
+
+                print(f"Consumed: ({msg_decoded})")
+        finally:
+            await self.consumer.stop()
