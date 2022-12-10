929f591f9b9d47b8b1d1bc9b7a08407badc0ac8e
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-12-09 23:07:38 -0300

    docs: instructions

--- console/src/console/scehmas/instruction.py
+++ console/src/console/scehmas/instruction.py
@@ -8,7 +8,14 @@ from .base import CamelCaseModel
 
 class InstructionType(str, Enum):
     """
-    Instruction Type Enum
+    Different instructions which will be sent to a message queue.
+
+    EXIT    -> Only used to exit the program has no parameters
+    NO_OP   -> No operation (1) [number of no operations]
+    IO      -> Simulates an IO operation (1)  [the time in milliseconds]
+    READ    -> Reads a value from a register (1) [memory address location]
+    WRITE   -> Writes a value to a register (2) [value, memory address location]
+    COPY    -> Copies a value from one register to another (2) [source address location, destination address location]
     """
     EXIT = 'EXIT'
     READ = 'READ'
@@ -20,7 +27,7 @@ class InstructionType(str, Enum):
 
 class Instruction(CamelCaseModel):
     """
-    Instruction Model
+    Instruction Model. See InstructionType for more information about parameters.
     """
     name: InstructionType
     params: list[int] = []
