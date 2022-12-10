eb12bb5643d05d285f8a55910e5616708ae01653
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-12-10 00:13:00 -0300

    feat: add upload endpoint

--- console/src/console/dependencies.py
+++ console/src/console/dependencies.py
@@ -1,5 +1,10 @@
+from src.console.services.file_parser_service import FileParserService
 from src.console.services.kafka_service import KafkaService
 
 
 async def get_kafka_service():
     return KafkaService()
+
+
+async def get_parser_service():
+    return FileParserService()
--- console/src/console/routers/instructions.py
+++ console/src/console/routers/instructions.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
-from src.console.dependencies import get_kafka_service
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
+from src.console.dependencies import get_kafka_service, get_parser_service
 from src.console.scehmas.instruction import Instruction
+from src.console.services.file_parser_service import FileParserService
 from src.console.services.kafka_service import KafkaService
 
 router = APIRouter(
@@ -13,9 +14,46 @@ router = APIRouter(
 @router.post("/")
 async def create_instruction(instructions: list[Instruction], ks: KafkaService = Depends(get_kafka_service)):
     """
-    Publishes an instruction into a message queue. It will be consumed by a Kernel consumer.
+    Publishes a list of instructions into a message queue. It will be consumed by a Kernel consumer.
     """
     try:
+        exit_instruction = Instruction(name="EXIT")
+
+        if exit_instruction not in instructions:
+            instructions.append(exit_instruction)
+        else:
+            instructions = instructions[:instructions.index(exit_instruction) + 1]
+
         await ks.publish(instructions)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/upload",
+             response_model=list[Instruction],
+             responses={400: {"description": "Invalid file content"}},
+             response_description="A parsed list of instructions")
+async def push_instructions_from_file(file: UploadFile, parser: FileParserService = Depends(get_parser_service)):
+    """
+    Loads a file with instructions and publishes them into a message queue. It will be consumed by a Kernel consumer.
+
+    It must be a txt file in which each line represent an instruction. EXIT must be the last instruction. e.g:
+
+    NO_OP 1
+
+    WRITE 2 3
+
+    READ 3
+
+    COPY 3 4
+
+    EXIT
+
+    """
+    try:
+        instructions = await parser.parse(file)
+        return instructions
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
